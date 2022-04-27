78843e258f6cd7c8172c11aed22029d39449077b
dj-viking <anders.swedishviking@gmail.com>
2022-04-27 14:51:13 -0400

    okay all the sudden it works now some how, this makes no sense, it was half working when i removed the context property in the proxy.conf.js, and then suddenly nothing worked. And then adding the context property back in because I got an http-proxy package error, so I put the context back in and then every page works now, I don't think I will ever understand why it broke or why it fixed it self, this is very unstable

--- ClientApp/proxy.conf.js
+++ ClientApp/proxy.conf.js
@@ -5,6 +5,10 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 const PROXY_CONFIG = [
   {
+    context: [
+      "/weatherforecast",
+      "/todos"
+    ],
     target: target,
     secure: false,
     headers: {
