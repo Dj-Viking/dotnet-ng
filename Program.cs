77ac8f26eec33a8153299665335c327b3415b16c
dj-viking <anders.swedishviking@gmail.com>
2022-05-16 00:22:34 -0400

    remove type inferred vars

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.Net.Http.Headers;
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
                       });
 });
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
