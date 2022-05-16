5c20cbb8adeca0d5c6bd964be3fac3116a6f215e
dj-viking <anders.swedishviking@gmail.com>
2022-05-16 07:00:25 -0400

    able to get a token string from hard coded user login info

---
+++ Models/UserLogin.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_ng;
+
+public class UserLogin
+{
+    public string? username { get; set; }
+    public string? password { get; set; }
+}
