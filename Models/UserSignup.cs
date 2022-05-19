75df78a1321fb58f188213165eaa8bf27391d994
dj-viking <anders.swedishviking@gmail.com>
2022-05-18 22:06:05 -0400

    remove fields from UserSignup class

--- Models/UserSignup.cs
+++ Models/UserSignup.cs
@@ -7,10 +7,7 @@ namespace dotnet_ng;
 
 public class UserSignup
 {
-    public int id { get; set; }
     public string? username { get; set; }
     public string? user_pass { get; set; }
     public string? email { get; set; }
-    public string? user_role { get; set; }
-    public string? token { get; set; }
 }
